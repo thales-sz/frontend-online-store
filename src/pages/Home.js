d8c159c33f69819c1abd3965ecd069036620a8b4
Thales Chagas <thales.souz@outlook.com>
2022-05-24 17:07:05 -0300

    Requisito 4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import Category from '../components/category';
+import { getCategories } from '../services/api';
+import './Home.css';
 
 class Home extends React.Component {
+  state = {
+    categories: [],
+  }
+
+  componentDidMount = async () => {
+    const categories = await getCategories();
+    this.setState({ categories });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
-      <div data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </div>
+      <main>
+        <section className="category-container">
+          Categorias:
+          {' '}
+          {categories.map((item) => (<Category
+            id={ item.id }
+            name={ item.name }
+            key={ item.id }
+          />))}
+        </section>
+        <div data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </div>
+      </main>
     );
   }
 }
