c47cdf0139a177e3f80e68945b651777046135d6
Thales Chagas <thales.souz@outlook.com>
2022-05-24 15:20:58 -0300

    Iniciando o projeto - Requisito 01

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,21 @@
+// https://api.mercadolibre.com/sites/MLB/categories - categorias
+
 export async function getCategories() {
-  // Implemente aqui
+  try {
+    const responseAPI = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+    const result = await responseAPI.json();
+    return result;
+  } catch (e) {
+    throw new Error('Failed to fetch');
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  try {
+    const responseAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
+    const result = await responseAPI.json();
+    return result;
+  } catch (error) {
+    throw new Error('Failed to fetch');
+  }
 }
