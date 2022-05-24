f009c4e59a2a4eb2e40aea8ae630a0cb54a644d8
Pedro de Paula França <pedrodepaulafranca@gmail.com>
2022-05-24 16:55:40 -0300

    inclui requisito 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
     return (
-      <div data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </div>
+      <>
+        <div data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </div>
+        <Link to="/shoppingcart" data-testid="shopping-cart-button">Ir para o carrinho</Link>
+      </>
     );
   }
 }
