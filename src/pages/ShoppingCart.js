75954eb97e97a3f5ac1b619edd72111d421098c7
Thales Chagas <thales.souz@outlook.com>
2022-05-30 15:33:10 -0300

    oi

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -3,64 +3,110 @@ import PropTypes from 'prop-types';
 
 let quantity = 0;
 
-// Eu implementei os 2 botoes de aumentar e diminuir a quantidade, tentei mudar o valor da 'quantity' direto na variavel mas n deu certo
 class ShoppingCart extends React.Component {
+  state={
+    productList: [],
+  }
+
+  componentDidMount = () => {
+    const productList = [];
+    const itemQuantity = [];
+    const { shopCartProducts } = this.props;
+    shopCartProducts.map((product, index) => {
+      quantity = 0;
+      const verifyProduct = product[0].title;
+      shopCartProducts.forEach((prod) => {
+        if (verifyProduct === prod[0].title) {
+          quantity += 1;
+        }
+      });
+      const prodObj = {
+        id: product[0].id,
+        title: product[0].title,
+        price: product[0].price,
+        quantity,
+      };
+      if (index === shopCartProducts.length - 1) {
+        this.setState({ productList });
+      }
+      if (itemQuantity.includes(verifyProduct)) {
+        return null;
+      }
+      productList.push(prodObj);
+      itemQuantity.push(verifyProduct);
+      return (
+        null
+      );
+    });
+  }
+
   increaseOrDecrease = ({ target }) => {
-    if (target.id === 'increase') {
-      quantity += 1;
+    const { productList } = this.state;
+    const changeQnt = productList.find((product) => product.id === target.id);
+    if (target.name === 'increase') {
+      productList[productList.indexOf(changeQnt)] = {
+        id: changeQnt.id,
+        title: changeQnt.title,
+        quantity: changeQnt.quantity + 1,
+        price: changeQnt.price,
+      };
     } else {
-      quantity -= 1;
-    } this.forceUpdate();
+      if (productList[productList.indexOf(changeQnt)].quantity === 1) {
+        productList[productList.indexOf(changeQnt)] = {
+          id: changeQnt.id,
+          title: changeQnt.title,
+          quantity: 1,
+          price: changeQnt.price,
+        };
+      }
+      productList[productList.indexOf(changeQnt)] = {
+        id: changeQnt.id,
+        title: changeQnt.title,
+        quantity: changeQnt.quantity - 1,
+        price: changeQnt.price,
+      };
+    }
+    this.setState({ productList });
   }
 
   render() {
-    const itemQuantity = [];
     const { shopCartProducts } = this.props;
+    const { productList } = this.state;
     return (
       <section>
         {shopCartProducts.length ? (
-          shopCartProducts.map((product) => {
-            const verifyProduct = product[0].title;
-            shopCartProducts.forEach((prod) => {
-              if (verifyProduct === prod[0].title) {
-                quantity += 1;
-              }
-            });
-            if (itemQuantity.includes(verifyProduct)) {
-              return null;
-            }
-            itemQuantity.push(verifyProduct);
-            return (
-              <div
-                className="shopcart-product"
-                key={ product[0].id }
-              >
-                <p data-testid="shopping-cart-product-name">
-                  {product[0].title}
-                </p>
-                <p data-testid="shopping-cart-product-quantity">{quantity}</p>
-                <p>{product[0].price * quantity}</p>
-                <div className="quantity-buttons">
-                  <button
-                    type="button"
-                    onClick={ this.increaseOrDecrease }
-                    id="decrease"
-                    data-testid="product-decrease-quantity"
-                  >
-                    -
-                  </button>
-                  <button
-                    type="button"
-                    onClick={ this.increaseOrDecrease }
-                    id="increase"
-                    data-testid="product-increase-quantity"
-                  >
-                    +
-                  </button>
-                </div>
+          productList.map((product) => (
+            <div
+              className="shopcart-product"
+              key={ product.id }
+            >
+              <p data-testid="shopping-cart-product-name">
+                {product.title}
+              </p>
+              <p data-testid="shopping-cart-product-quantity">{product.quantity}</p>
+              <p>{product.price * product.quantity}</p>
+              <div className="quantity-buttons">
+                <button
+                  type="button"
+                  name="decrease"
+                  id={ product.id }
+                  onClick={ this.increaseOrDecrease }
+                  data-testid="product-decrease-quantity"
+                >
+                  -
+                </button>
+                <button
+                  type="button"
+                  name="increase"
+                  onClick={ this.increaseOrDecrease }
+                  id={ product.id }
+                  data-testid="product-increase-quantity"
+                >
+                  +
+                </button>
               </div>
-            );
-          })
+            </div>
+          ))
         )
           : (
             <div data-testid="shopping-cart-empty-message">
